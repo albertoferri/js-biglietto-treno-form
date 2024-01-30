3752e99b99e350055ef0f6c2fb13b80bcf0dbdc8
albertoferri <falberto99@gmail.com>
2024-01-30 17:29:58 +0100

    errore di conversione stringa a numero

--- js/script.js
+++ js/script.js
@@ -2,18 +2,25 @@
 const inputElement = document.querySelector("#input-km");
 const ageInputEl = document.querySelector("#age")
 const buttonElement = document.querySelector("#invia");
+const basePrice = inputElement * 0.21;
+
+// trasformo le variabili ottenute in numeri
+let kmNumber = Number(inputElement);
 
 
 
 buttonElement.addEventListener('click',
     function() {
 
+        
+
         document.querySelector("#risposte p:first-child").innerHTML = "LA TUA DISTANZA E' DI " + inputElement.value + " km";
         document.querySelector("#risposte p:nth-of-type(2)").innerHTML = "HAI " + ageInputEl.value + " ANNI";
         
         
-        console.log(inputElement.value);
+        console.log(kmNumber);
         console.log(ageInputEl.value);
+        // console.log(basePrice.value)
         
     }
-)
+    )
