7d95f6e1573ed72ca3eaf0b6e9ddef2e316546be
albertoferri <falberto99@gmail.com>
2024-01-30 16:39:46 +0100

    risolto errore di sintassi e log console

--- js/script.js
+++ js/script.js
@@ -7,11 +7,13 @@ const buttonElement = document.querySelector("#invia");
 
 buttonElement.addEventListener('click',
     function() {
-        
-        document.querySelector("#risposte").innerHTML = "LA TUA DISTANZA E' DI " + inputElement;
 
-        console.log(inputElement.innerHTML);
-        console.log(ageInputEl.innerHTML);
+        document.querySelector("#risposte p:first-child").innerHTML = "LA TUA DISTANZA E' DI " + inputElement.value + " km";
+        document.querySelector("#risposte p:nth-of-type(2)").innerHTML = "HAI " + ageInputEl.value + " ANNI";
+        
+        
+        console.log(inputElement.value);
+        console.log(ageInputEl.value);
         
     }
 )
