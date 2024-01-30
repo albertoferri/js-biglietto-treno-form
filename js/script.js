16de9dbb6895c933f524b39a9b11fbbd236d3a03
albertoferri <falberto99@gmail.com>
2024-01-30 18:16:03 +0100

    milestone 1 finita

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
 // memorizzo campo di input
-const inputElement = document.querySelector("#input-km");
+const inputKmElement = document.querySelector("#input-km");
 const ageInputEl = document.querySelector("#age")
 const buttonElement = document.querySelector("#invia");
-const basePrice = inputElement * 0.21;
-
-// trasformo le variabili ottenute in numeri
-let kmNumber = Number(inputElement);
-
-
 
 buttonElement.addEventListener('click',
-    function() {
+function() {
+    
+    // trasformo le variabili ottenute in numeri
+    let km = inputKmElement.value;
+    let ageV = ageInputEl.value;
+    let basePrice = inputKmElement.value * 0.21;
+    
+    // arrotondo i risultati lasciando due decimali
+    let priceYoung = (basePrice- (basePrice * 0.2)).toFixed(2)
+    let priceSenior = (basePrice- (basePrice * 0.4)).toFixed(2)
 
-        
 
-        document.querySelector("#risposte p:first-child").innerHTML = "LA TUA DISTANZA E' DI " + inputElement.value + " km";
-        document.querySelector("#risposte p:nth-of-type(2)").innerHTML = "HAI " + ageInputEl.value + " ANNI";
-        
-        
-        console.log(kmNumber);
-        console.log(ageInputEl.value);
-        // console.log(basePrice.value)
+    document.querySelector("#risposte p:first-child").innerHTML = "LA TUA DISTANZA E' DI " + km + " km";
+    document.querySelector("#risposte p:nth-of-type(2)").innerHTML = "HAI " + ageV + " ANNI";
+    document.querySelector("#risposte p:last-child").innerHTML = "LA TARIFFA BASE è DI €" + basePrice;
+    
+    if (ageV < 18){
+        document.querySelector("#risposte p:last-child").innerHTML = "DATO CHE RIENTRI NELLA TARIFFA YOUNG IL TUO PREZZO È DI €" + priceYoung;
+    } else if (ageV > 64){
+        document.querySelector("#risposte p:last-child").innerHTML = "DATO CHE RIENTRI NELLA TARIFFA SENIOR IL TUO PREZZO È DI €" + priceSenior;
+    }
+
+
+
+    
+        console.log("LA TUA DISTANZA E' DI " + km + " km");
+        console.log("HAI " + ageV + " ANNI");
+        console.log("LA TARIFFA BASE è DI €" + basePrice)
+        console.log("LA TARIFFA YOUNG è DI €" + priceYoung)
+        console.log("LA TARIFFA SENIOR è DI €" + priceSenior)
         
     }
     )
