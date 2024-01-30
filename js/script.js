8754fffe0997fa234bcfc1357ed310700546a457
albertoferri <falberto99@gmail.com>
2024-01-30 20:27:44 +0100

    prima stilizzazione form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // memorizzo campo di input
+const userName = document.querySelector("#name-user");
 const inputKmElement = document.querySelector("#input-km");
 const ageInputEl = document.querySelector("#age")
 const buttonElement = document.querySelector("#invia");
@@ -16,17 +17,31 @@ function() {
     let priceSenior = (basePrice- (basePrice * 0.4)).toFixed(2)
 
 
-    document.querySelector("#risposte p:first-child").innerHTML = "LA TUA DISTANZA E' DI " + km + " km";
-    document.querySelector("#risposte p:nth-of-type(2)").innerHTML = "HAI " + ageV + " ANNI";
-    document.querySelector("#risposte p:last-child").innerHTML = "LA TARIFFA BASE è DI €" + basePrice;
+    // document.querySelector("#risposte p:first-child").innerHTML = "LA TUA DISTANZA E' DI " + km + " km";
+    // document.querySelector("#risposte p:nth-of-type(2)").innerHTML = "HAI " + ageV + " ANNI";
+    // document.querySelector("#risposte p:last-child").innerHTML = "LA TARIFFA BASE è DI €" + basePrice;
     
+    // applico sconto in base all'età
     if (ageV < 18){
-        document.querySelector("#risposte p:last-child").innerHTML = "DATO CHE RIENTRI NELLA TARIFFA YOUNG IL TUO PREZZO È DI €" + priceYoung;
+        document.querySelector(".biglietto p:last-child #pricePrint").innerText = "€ " + priceYoung;
+        document.querySelector('#offerta').innerText = 'Biglietto Young';
+
     } else if (ageV > 64){
-        document.querySelector("#risposte p:last-child").innerHTML = "DATO CHE RIENTRI NELLA TARIFFA SENIOR IL TUO PREZZO È DI €" + priceSenior;
+        document.querySelector(".biglietto p:last-child #pricePrint").innerText = "€ " + priceSenior;
+        document.querySelector("#offerta").innerText = 'Biglietto Senior';
+
+    } else{
+        document.querySelector('#pricePrint').innerText = "€ " + basePrice;
+        document.querySelector('#offerta').innerText = 'Biglietto Standard';
     }
 
 
+    document.getElementById("yourTicket").innerHTML = "IL TUO BIGLIETTO";
+    document.querySelector('#nome').innerText = userName.value;
+    document.querySelector('#carrozza').innerText = '5';
+    document.querySelector('#codice').innerText = '92911';
+    
+    
 
     
         console.log("LA TUA DISTANZA E' DI " + km + " km");
